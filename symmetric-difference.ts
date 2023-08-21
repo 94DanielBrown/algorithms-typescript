44002e7869700908b02c3dc43fb149d5a42c546a
Daniel Brown <94daniel.brown@gmail.com>
2023-08-21 13:23:53 +0100

    updated solution

--- symmetric-difference.ts
+++ symmetric-difference.ts
@@ -1,13 +1,21 @@
-const sym = (...args: number[][]) => {
-    const concatenatedArray: number[] = [].concat(...args);
-    const symmetricDifference: number[] = concatenatedArray.filter((element,index,array) => {
-        return array.indexOf(element) === array.lastIndexOf(element);
-    })
-    console.log(symmetricDifference);
+// BELOW ONLY WORKS for 2 arrays
+// const sym = (...args: number[][]) => {
+//     const concatenatedArray: number[] = [].concat(...args);
+//     const symmetricDifference: number[] = concatenatedArray.filter((element,index,array) => {
+//         return array.indexOf(element) === array.lastIndexOf(element);
+//     })
+//     console.log(symmetricDifference);
+// }
+const sym = (...args) => [...new Set(args.reduce(diff))];
+const app = () => {
+    const result = sym([1,2,3,3],[5,2,1,4]);
+    console.log(result);
 }
 
-const app = () => {
-    sym([1,2,3],[5,2,1,4]);
-}
+const diff = (arr1, arr2) => [
+    ...arr1.filter(e => !arr2.includes(e)),
+    ...arr2.filter(e => !arr1.includes(e))
+];
 
 app();
+
