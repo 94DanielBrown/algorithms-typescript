02a7afd8f91c64edfb24e9b19a430f88b4bac9ae
Daniel Brown <94daniel.brown@gmail.com>
2023-08-22 10:55:11 +0100

    update

--- pairwise/pairwise.ts
+++ pairwise/pairwise.ts
@@ -1,23 +1,18 @@
-const pairwise = (arr: number[], arg: number[]): number => {
+const pairwise = (arr: number[], arg: number): number => {
     const indexMap: Record<number, number> = {};
 
-    let totalSum =0;
+    let totalSum = 0;
 
     arr.forEach((num, index) => {
         const complement: number = arg - num;
 
-        if (indexMap.hasOwnProperty((complement)) {
-
+        if (indexMap.hasOwnProperty(complement)) {
+            totalSum += index + indexMap[complement];
+            delete indexMap[complement];
+        } else {
+            indexMap[num] = index;
         }
+    });
 
-
-
-
-
-    })
-
-
-
-
-    return totalSum
-}
+    return totalSum;
+};
